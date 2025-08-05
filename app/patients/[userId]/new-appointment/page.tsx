4cc4eb660695f969b9c122a7572a2ec4db6b4823
John muller <phychicjosh@gmail.com>
2025-08-05 09:40:57 +0300

    resolved the error

--- app/patients/[userId]/new-appointment/page.tsx
+++ app/patients/[userId]/new-appointment/page.tsx
@@ -1,36 +1,9 @@
-import Image from "next/image";
+import React from "react";
 import AppointmentForm from "@/components/forms/AppointmentForm";
-import { getPatient } from "@/lib/actions/patient.actions";
 
-export default async function NewAppointment({
-  params: { userId },
-}: SearchParamProps) {
-  const patient = await getPatient(userId);
+const Appointment = () => {
   return (
-    <div className={"flex h-screen max-h-screen"}>
-      <section className={"remove-scrollbar container my-auto"}>
-        <div className={"sub-container max-w-[860px] flex-1 justify-between"}>
-          <Image
-            src={"/assets/icons/logo-full.svg"}
-            height={1000}
-            width={1000}
-            alt={"patient"}
-            className={"mb-12 h-10 w-fit"}
-          />
-
-          <AppointmentForm type={"create"} userId={userId} />
-
-          <p className={"copyright mt-10 py-1"}>©copyright carepulse</p>
-        </div>
-      </section>
-
-      <Image
-        src={"/assets/images/appointment-img.png"}
-        alt={"appointment"}
-        height={1000}
-        width={1000}
-        className={"side-img max-w-[390px] bg-bottom"}
-      />
-    </div>
+    <div>Appointment</div>
   );
-}
+};
+export default Appointment;
