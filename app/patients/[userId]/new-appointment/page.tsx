21d175bee10ce3c3579cab36b249ceb2b55ffd75
John muller <phychicjosh@gmail.com>
2025-08-07 06:50:58 +0300

    to check that the origin master is the same as the local

--- app/patients/[userId]/new-appointment/page.tsx
+++ app/patients/[userId]/new-appointment/page.tsx
@@ -2,8 +2,6 @@ import React from "react";
 import AppointmentForm from "@/components/forms/AppointmentForm";
 
 const Appointment = () => {
-  return (
-    <div>Appointment</div>
-  );
+  return <AppointmentForm />;
 };
 export default Appointment;
